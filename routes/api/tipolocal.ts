80f42586f03b4304e90a42dc02dce063580f391b
andrematech <amarchesini89@gmail.com>
2019-05-31 15:53:09 -0300

    model, rotas, view do tipo local! falta app.ts

---
+++ routes/api/tipolocal.ts
@@ -0,0 +1,50 @@
+﻿import express = require("express");
+import wrap = require("express-async-error-wrapper");
+import jsonRes = require("../../utils/jsonRes");
+import Usuario = require("../../models/usuario");
+import TipoLocal = require("../../models/tipolocal");
+
+const router = express.Router();
+
+router.get("/listar", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req, res);
+    if (!u)
+        return;
+    res.json(await TipoLocal.listar());
+}));
+
+router.get("/obter", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req, res);
+    if (!u)
+        return;
+    let id_tipo_local = parseInt(req.query["id_tipo_local"]);
+    res.json(isNaN(id_tipo_local) ? null : await TipoLocal.obter(id_tipo_local));
+}));
+
+router.post("/criar", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req, res, true);
+    if (!u)
+        return;
+    let s = req.body as TipoLocal;
+    jsonRes(res, 400, s ? await TipoLocal.criar(s) : "Dados inválidos!");
+}));
+
+router.post("/alterar", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req, res, true);
+    if (!u)
+        return;
+    let s = req.body as TipoLocal;
+    if (s)
+        s.id_tipo_local = parseInt(req.body.id_solucao);
+    jsonRes(res, 400, (s && !isNaN(s.id_tipo_local)) ? await TipoLocal.alterar(s) : "Dados inválidos!");
+}));
+
+router.get("/excluir", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req, res, true);
+    if (!u)
+        return;
+    let id_tipo_local = parseInt(req.query["id_tipo_local"]);
+    jsonRes(res, 400, isNaN(id_tipo_local) ? "Dados inválidos!" : await TipoLocal.excluir(id_tipo_local));
+}));
+
+export = router;
---
+++ routes/tipolocal.ts
@@ -0,0 +1,52 @@
+﻿import express = require("express");
+import wrap = require("express-async-error-wrapper");
+import Usuario = require("../models/usuario");
+import TipoLocal = require("../models/tipolocal");
+
+const router = express.Router();
+
+router.all("/criar", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req);
+    if (!u || !u.admin) {
+        res.redirect("/acesso");
+    } else {
+        res.render("controle/tipolocal/alterar", {
+            titulo: "Criar Locacl",
+            usuario: u,
+            item: null
+        });
+    }
+}));
+
+router.all("/alterar", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req);
+    if (!u || !u.admin) {
+        res.redirect("/acesso");
+    } else {
+        let id_tipo_local = parseInt(req.query["id_tipo_local"]);
+        let item: TipoLocal = null;
+        if (isNaN(id_tipo_local) || !(item = await TipoLocal.obter(id_tipo_local)))
+            res.render("shared/nao-encontrado", { usuario: u });
+        else
+            res.render("controle/solucao/alterar", {
+                titulo: "Editar Local",
+                usuario: u,
+                item: item
+            });
+    }
+}));
+
+router.get("/listar", wrap(async (req: express.Request, res: express.Response) => {
+    let u = await Usuario.cookie(req);
+    if (!u || !u.admin) {
+        res.redirect("/acesso");
+    } else {
+        res.render("controle/tipolocal/listar", {
+            titulo: "Gerenciar Locais",
+            usuario: u,
+            lista: JSON.stringify(await TipoLocal.listar())
+        });
+    }
+}));
+
+export = router;
